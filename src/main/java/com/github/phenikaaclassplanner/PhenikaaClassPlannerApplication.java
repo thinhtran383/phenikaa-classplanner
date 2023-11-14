87c9ecd3bf06d6c69bac6ddd8ae3372de9b5b4b7
ThinhTran <thinhtran3803@gmail.com>
2023-11-14 09:08:26 +0700

    init project

---
+++ src/main/java/com/github/phenikaaclassplanner/PhenikaaClassPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.github.phenikaaclassplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhenikaaClassPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhenikaaClassPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/phenikaaclassplanner/PhenikaaClassPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.phenikaaclassplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhenikaaClassPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
